6b3d3c8790c75c1da6b9b2a3bac4f72877ca6fbe
will <will@heueso.com>
2021-01-27 11:53:35 +0800

    basic exec flow sans instruction handling

--- toy-vm/toy-vm.c
+++ toy-vm/toy-vm.c
@@ -3,27 +3,47 @@
 
 //instruction set
 # define LOAD_WORD 0x01
-# define STORE_WORD -x02
+# define STORE_WORD 0x02
 # define ADD 0x03
 # define SUB 0x04
 # define HALT 0xff
 
-//locations
+//internal state
 # define PROGRAM_COUNTER 0x00
 # define REG_1 0x01
 # define REG_2 0x02
 
-void vm(int memory[]) {
+//program
+# define PROGRAM_LENGTH 14
 
+void load_word(int reg_1, int addr ) {
+
+}
+
+void vm(int memory[]) {
+    int registers[3];
+    registers[PROGRAM_COUNTER] = 0x00;
+
+    while(registers[PROGRAM_COUNTER] < 14) {
+        int current_instruction_address = registers[PROGRAM_COUNTER];
+
+
+
+        if (current_instruction_address == HALT) {
+            break;
+        }
+
+        registers[PROGRAM_COUNTER] += 3;
+    }
 }
 
 void main() {
     int program[20] = {
-        0x01, 0x02, 0x12,
-        0x03, 0x01, 0x02,
-        0x01, 0x01, 0x10,
-        0x02, 0x01, 0x0e,
-        0xff,
+        LOAD_WORD, REG_1, 0x10,
+        LOAD_WORD, REG_2, 0x12,
+        ADD, REG_1, REG_2,
+        STORE_WORD, REG_1, 0x0e,
+        HALT,
         0x00,
         0x00, 0x00,
         0xa1, 0x14,
@@ -32,5 +52,3 @@ void main() {
 
     vm(program);
 }
-
-
