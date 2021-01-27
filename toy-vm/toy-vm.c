c468a1eac89b6b41142a686d08b4ccef935ee8c2
will <will@heueso.com>
2021-01-27 11:21:22 +0800

    remove until successful compile

--- toy-vm/toy-vm.c
+++ toy-vm/toy-vm.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+//instruction set
+# define LOAD_WORD 0x01
+# define STORE_WORD -x02
+# define ADD 0x03
+# define SUB 0x04
+# define HALT 0xff
+
+//locations
+# define PROGRAM_COUNTER 0x00
+# define REG_1 0x01
+# define REG_2 0x02
+
+void vm(int memory[]) {
+
+}
+
+void main() {
     int program[20] = {
         0x01, 0x02, 0x12,
         0x03, 0x01, 0x02,
@@ -13,52 +29,8 @@ int main() {
         0xa1, 0x14,
         0x0c, 0x00
     };
-   
 
-   void load_word(int reg, int mem_address);
-   void store_word(int reg, int mem_address);
-   void add_words(int reg_1, int reg_2);
-   void subtract_words(int reg_1, int reg_2);
-   void halt();
-   void virtual_computer(int program[]);
+    vm(program);
+}
 
-   void virtual_computer(program) {
-        int registry[3] = {0x00, 0x00, 0x00};
-        
-        int current_instruction = program[registry[0x00]];
-        while(current_instruction != HALT && registry[0x00] < 20) {
-        
 
-            case(current_instruction) {
-                case 0x01:
-                    register_number = program[current_instruction + 0x01];
-                    input_value = program[current_instruction + 0x02];
-                    load_word(register_number, input_value);
-                    break;
-                case 0x02: 
-                    register_number = program[current_instruction + 0x01];
-                    input_value = program[current_instruction + 0x02];
-                    store_word(register_number, input_value);
-                    break;
-                case 0x03:
-                    int addend_1_location = program[current_instruction + 0x01];
-                    int addend_2_location = program[current_instruction + 0x02];
-                    add(addend_1_location, addend_2_location);
-                    break;
-                case 0x04: 
-                    int minuend = program[current_instruction + 0x01];
-                    int subtrahend = program[current_instruction + 0x02];
-                    subtract(minuend, subtrahend);
-                    break;
-                case 0xff: 
-                    halt();
-                    break;
-            };
-
-            registry[0x00]++;
-            current_instruction = program[registry[0x00]];
-        };
-
-        return void;
-   };
-};
